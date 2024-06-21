2947e23613e405086f0420cccca5bbdfab838e90
Ayush <ayushmishra2854@gmail.com>
2024-06-22 04:12:31 +0530

    added hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState<string>("");
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 2,
+    });
+
+    gsap.to("#cta", {
+      opacity: 1,
+      delay: 2,
+      y: -50,
+    });
+  }, []);
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc("/assets/videos/smallHero.mp4");
+    } else {
+      setVideoSrc("/assets/videos/hero.mp4");
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth < 760) {
+      setVideoSrc("/assets/videos/smallHero.mp4");
+    } else {
+      setVideoSrc("/assets/videos/hero.mp4");
+    }
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  return (
+    <section className="nav-height relative w-full bg-black">
+      <div className="flex-center h-5/6 w-full flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex translate-y-20 flex-col items-center opacity-0"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="text-xl font-normal">From $199/month or $999</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable tailwindcss/enforces-shorthand */
-/* eslint-disable tailwindcss/classnames-order */
 import { navLists } from "@/constants";
 import { appleImg, bagImg, searchImg } from "@/utils";
 import Image from "next/image";
@@ -7,20 +5,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="flex w-full items-center justify-between py-5 sm:px-10 px-5">
-      <nav className="flex w-full screen-max-width">
+    <header className="flex w-full items-center justify-between p-5 sm:px-10">
+      <nav className="screen-max-width flex w-full">
         <Image src={appleImg} alt="Apple logo" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
             <div
-              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              className="cursor-pointer px-5 text-sm text-gray transition-all hover:text-white"
               key={nav}
             >
               {nav}
             </div>
           ))}
         </div>
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
           <Image src={searchImg} alt="Apple logo" width={18} height={18} />
           <Image src={bagImg} alt="Apple logo" width={18} height={18} />
         </div>
