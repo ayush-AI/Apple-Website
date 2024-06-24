e93944c71bf91fd42db6046b3c8b2033df399cde
Ayush <ayushmishra2854@gmail.com>
2024-06-24 17:07:04 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Highlight from "@/components/Highlight";
 import HowItWorks from "@/components/HowItWorks";
@@ -15,6 +16,7 @@ export default function Home() {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 }
